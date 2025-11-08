7e2cdba169a96bd385b2fc640de4f2c904985c83
gleici-franco <gleici15_@hotmail.com>
2025-11-08 17:12:25 +0000

    Implement coin flip slider and launch button
    Added slider for number of coin flips and button to start experiment.

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.header('Jogando uma moeda')
 
-st.write('Ainda não é um aplicativo funcional. Em construção.')
+# Controle deslizante para número de lançamentos
+num_flips = st.slider('Número de lançamentos:', min_value=1, max_value=1000, value=100)
+
+# Botão para iniciar o experimento
+if st.button('Lançar moedas!'):
+    st.write(f'Lançando a moeda {num_flips} vezes...')
